aadf9f1053383709ebfa07de8eb860f4fe5b7bfb
Akshay S Udupa <akshayudupa@hotmail.com>
2022-03-25 17:17:30 -0500

    New updates for hello_world and trial file

--- src/hello_world.py
+++ src/hello_world.py
@@ -3,3 +3,5 @@ print("now, please add more functionality to this codebase!")
 
 print(2*4)
 print('Understanding Git')
+
+print('Making it easy')
--- trial/test_1.py
+++ trial/test_1.py
@@ -2,3 +2,17 @@ def add_num(n1,n2):
     return n1+n2
 
 print(add_num(5+4))
+
+def merge_word(s1,s2):
+    return ('s1' +' s2')
+
+print(merge_word('Git', "Hub"))
+
+def even_nums(n):
+    if n%2==0:
+        return 'Even'
+    else:
+        return 'Odd'
+
+print(even_nums(15))
+
